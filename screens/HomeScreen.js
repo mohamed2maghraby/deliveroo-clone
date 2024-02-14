cbd68c609df2cc08137ba346d0834efb22fd4366
mohamed maghraby <mohamed.maghraby@artlinkgroup.com>
2024-02-14 16:07:00 +0200

    first commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,43 @@
+import { View, Text, SafeAreaView, Image } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import {useNavigation } from '@react-navigation/native'
+import {
+    UserIcon,
+    ChevronDownIcon,
+    SearchIcon,
+    AdjustmentsIcon
+} from 'react-native-heroicons/outline'
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => { //like useEffect
+        navigation.setOptions({
+            headerShown: false,
+        })
+    }, [])
+  return (
+    <SafeAreaView>
+      <Text className='text-red-500'>
+        <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+            <Image
+                source={{ 
+                    url: 'https://links.papareact.com/wru'
+                }}
+                className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+            />
+            <View>
+                <Text className="font-bold text-gray-400 text-xs">
+                    Deliver Now!
+                </Text>
+                <Text className="font-bold text-xl">Current Location
+                    <ChevronDownIcon size={20} color="#00CCBB" />
+                </Text>
+            </View>
+        </View>
+      </Text>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
